9bc5f46636370d351f5e7c7bd56f2aae9efff4a4
Onur Melikoğlu <60974044+OBSoft-Onur@users.noreply.github.com>
2023-12-09 17:23:01 +0300

    signup validation fix

--- groceryshopping/lib/ui/views/signuppage.dart
+++ groceryshopping/lib/ui/views/signuppage.dart
@@ -19,6 +19,8 @@ class _SignUpPageState extends State<SignUpPage> {
   var tcEmail = TextEditingController();
   var tcPassword = TextEditingController();
   bool isPasswordVisible = false;
+  bool isLoading = false;
+  final formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
@@ -30,72 +32,109 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
+    var screenInfo = MediaQuery.of(context);
+    var screenWidth = screenInfo.size.width;
+    var screenHeight = screenInfo.size.height;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(left: 40, right: 40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            smallText(text: "Kayıt Ol", fontsize: 24, color: mediumColor, fontweight: FontWeight.w600,),
-            TextField(
-              controller: tcUsername,
-              decoration: const InputDecoration(labelText: "Kullanıcı Adı"),
-            ),
-            TextField(
-              controller: tcEmail,
-              decoration: const InputDecoration(labelText: "Email"),
-            ),
-            TextField(
-              controller: tcPassword,
-              decoration: InputDecoration(
-                  labelText: "Şifre",
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                      color: lightColor,
-                    ),
-                    onPressed: () {
-                      setState(() { isPasswordVisible = !isPasswordVisible; });
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: screenWidth,
+            height: screenHeight,
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  smallText(text: "Kayıt Ol", fontsize: 24, color: mediumColor, fontweight: FontWeight.w600,),
+                  TextFormField(
+                    controller: tcUsername,
+                    decoration: const InputDecoration(labelText: "Kullanıcı Adı"),
+                    validator: (value) {
+                      if (value!.isEmpty || !RegExp(r'^[a-z A-Z]+$').hasMatch(value!)) {
+                        return 'Lütfen Boş Alan Bırakmayınız';
+                      }
+                      return null;
                     },
-                  )
-              ),
-              obscureText: !isPasswordVisible,
-            ),
-            ElevatedButton(onPressed: (){
-              context.read<SignUpPageCubit>().signUp(tcUsername.text, tcEmail.text, tcPassword.text);
-            }, child: const Text("Kayıt ol")),
-            customButton(
-              text: "Kayıt Ol",
-              onPressed: (){
-                context.read<SignUpPageCubit>().signUp(tcUsername.text, tcEmail.text, tcPassword.text);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    backgroundColor: primaryColor,
-                    content: smallText(text: "Kayıt Başarılı! Giriş yapabilirsiniz.", fontsize: 16, color: Colors.white),
-                    duration: const Duration(seconds: 3),
                   ),
-                );
-                Navigator.pushReplacement(context, MaterialPageRoute(
-                    builder: (context) => const LoginPage()));
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                smallText(text: "Hesabınız var mı?", fontsize: 14, color: mediumColor, fontweight: FontWeight.w600,),
-                TextButton(
-                    onPressed: (){
-                      Navigator.pushReplacement(context, MaterialPageRoute(
-                        builder: (context) => const LoginPage()));
+                  TextFormField(
+                    controller: tcEmail,
+                    decoration: const InputDecoration(labelText: "Email"),
+                    validator: (value) {
+                      if (value!.isEmpty || !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!)) {
+                        return 'Lütfen Email Giriniz';
+                      }
+                      return null;
                     },
-                    child: smallText(text: "Giriş Yap", fontsize: 14, color: primaryColor, fontweight: FontWeight.w600,),
-                )
-              ],
-            )
-          ],
+                  ),
+                  TextFormField(
+                    controller: tcPassword,
+                    decoration: InputDecoration(
+                        labelText: "Şifre",
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: lightColor,
+                          ),
+                          onPressed: () {
+                            setState(() { isPasswordVisible = !isPasswordVisible; });
+                          },
+                        )
+                    ),
+                    obscureText: !isPasswordVisible,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Lütfen Boş Alan Bırakmayınız';
+                      }
+                      if(value.length < 5){
+                        return 'En az 5 karakter uzunluğunda bir şifre giriniz';
+                      }
+                      return null;
+                    },
+
+                  ),
+                  customButton(
+                    text: "Kayıt Ol",
+                    onPressed: (){
+                      if(formKey.currentState!.validate()){
+                        context.read<SignUpPageCubit>().signUp(tcUsername.text, tcEmail.text, tcPassword.text).then((value){
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              backgroundColor: primaryColor,
+                              content: smallText(text: "Kayıt Başarılı! Giriş yapabilirsiniz.", fontsize: 16, color: Colors.white),
+                              duration: const Duration(seconds: 3),
+                            ),
+                          );
+                          isLoading = false;
+                          Navigator.pushReplacement(context, MaterialPageRoute(
+                              builder: (context) => const LoginPage()));
+                        });
+
+                      }
+
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      smallText(text: "Hesabınız var mı?", fontsize: 14, color: mediumColor, fontweight: FontWeight.w600,),
+                      TextButton(
+                          onPressed: (){
+                            Navigator.pushReplacement(context, MaterialPageRoute(
+                              builder: (context) => const LoginPage()));
+                          },
+                          child: smallText(text: "Giriş Yap", fontsize: 14, color: primaryColor, fontweight: FontWeight.w600,),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
